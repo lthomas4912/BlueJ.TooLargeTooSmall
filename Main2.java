e9b7ee43b6e8da2715594124f47d6591701f9f59
Leah <leah29726@gmail.com>
2019-02-06 17:43:15 -0500

    Guessing Game

---
+++ Main2.java
@@ -0,0 +1,29 @@
+
+
+/**
+ * Write a description of class Main2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main2{
+    public static void main(String[] args){
+        int size = 5;
+        for(int i = 0; i < size; i++){
+      
+      System.out.println("\n");
+    }
+}
+
+        
+    public static void printStars(int size){
+        int count = 0;
+        
+        while (count < size){
+            System.out.println("*");
+            count++;
+            
+        }
+    } 
+}
+    
---
+++ RandomNum.java
@@ -0,0 +1,86 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class RandomNum
+{
+    // instance variables - replace the example below with your own
+    
+  
+    
+
+    /**
+     * Constructor for objects of class Numbers
+     */
+    public RandomNum()
+    {
+        // initialise instance variables
+       
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void main(){
+       System.out.println("Enter a number between 1 and 30");
+       Scanner input = new Scanner(System.in);
+       int guess = input.nextInt();
+       Random rand = new Random();
+       int n = rand.nextInt(30) + 1;
+       System.out.println(n);
+       
+       int counter = 0;
+       
+      
+         if(guess > n) {
+           System.out.println("too large");
+           counter++;
+        } else if (guess < n){
+            System.out.println("too small");
+            counter++;
+        } else {
+            System.out.println("Correct Guess! You took " + counter + " tries.");
+            
+            
+        } 
+      
+       
+            
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+      
+       
+       
+     
+    
+        // put your code here
+        
+    
+
+}
+}
+
+
